9c8df66df642a94172b9ba4a5536f3a128c69438
Xu Dai <davidlinqqhr@gmail.com>
2018-10-08 14:29:19 -0700

    add implementation for frontend

--- smart_contracts/truffle-config.js
+++ smart_contracts/truffle-config.js
@@ -11,8 +11,24 @@
  *     gasPrice: 10000000000,
  *   },
  */
-
+var HDWalletProvider = require("truffle-hdwallet-provider")
+var mnemonic = "employ direct tenant initial naive have victory canoe forest attend symptom artwork"
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*"
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/9f1fb598e3424075854ee430b1b69a7e")
+      },
+      network_id: 4,
+      gas: 4500000,
+      gasPrice: 10000000000
+    }
+  }
 };
